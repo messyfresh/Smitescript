da03f9088e9caa3f1df8de140fa5d68da9168d77
messyfresh <messyfresh@gmail.com>
2015-01-19 21:10:10 -0600

    Fixed a few issues, better readme with clearer instructions and an example for creating a sessionId.

--- smitescript/README.md
+++ smitescript/README.md
@@ -1,4 +1,5 @@
 # Smitescript
+Its fixed now, I apologize for the broken update.
 A node.js library for making calls to the HiRez Smite API.
 
 # How to Use
@@ -10,18 +11,39 @@ Simply include smitescript in your code and start making calls.
 	var smite = require('smite');
 
 	// Declare your smite developer credentials.
-	smite.devId = 'XXXX';
-	smite.authId = 'INSERT YOUR PRIVATE DEVELOPER KEY HERE';
+	var devId = 'XXXX';
+	var authId = 'INSERT YOUR PRIVATE DEVELOPER KEY HERE';
 
 	// Create a session (Good for 15 min)
-	smite.createSession();
+	smite.createSession(devId, authKey);
 
 	// Example call to retrieve all gods in the game.
-	smite.getGods();
+	smite.getGods(devId, authKey, sessionId);
+	smite.getItems(devId, authKey, sessionId);
 
 	// This call will create a .json file in the local directory of the response.
 	// At  this point a simple fs.createReadStream can be used to read the contents or
 	// the contents of the file can be pushed into a mongoDB.
+
+Here is another example for creating a session:
+
+	var sessionId;
+	
+	try {
+		sessionId = fs.readFileSync("session.txt", "utf8");
+	} catch (e) {
+		if (e.code === "ENOENT") {
+			sessionId = createSession(devId, authKey);
+		}
+	}
+
+# Current Issues
+	The first initial call to create a session has to be made
+	by itself or setTimeout in order to ensure a valid sessionId
+	has had time to make it back from the server. This shouldn't
+	be a problem for webservers are you can just create a sessionID
+	at the first page load, but this is not a very good way of
+	achieving that since the API has call limits.
 
 # Currently implemented
 	- createSession
@@ -36,3 +58,6 @@ Simply include smitescript in your code and start making calls.
 	- Create functions for all of the API calls.
 	- Establish mechanism to check if a session is expired.
 	- Be Awesome!
+
+# Be sure and check out the package and hack away at it!
+# Let me know what you come up with.
--- smitescript/smite.js
+++ smitescript/smite.js
@@ -3,51 +3,28 @@
     request = require('request'),
     parser = require('JSONStream'), // Remember to put .parse(what to parse) here when this is called
     moment = require('moment'),
-    sessionId = fs.readFileSync('session.txt', 'utf8'),
-    utcTime = moment().utc().format("YYYYMMDDHHmmss");
-
-var devId = '';
-var authKey = '';
-var baseUri = 'http://api.smitegame.com/smiteapi.svc/';
-
-// Check if a session.txt file exists **Currently not working**
-function checkSession() {
-    if (fs.existsSync('session.txt')) {
-        return fs.readFileSync('session.txt', 'utf8');
-    }
-    else {
-        createSession();
-        checkSession();
-    }
-}
+    utcTime = moment().utc().format("YYYYMMDDHHmmss"),
+    baseUri = 'http://api.smitegame.com/smiteapi.svc/';
 
 // Creates a Session Signature that will be used for
 // every call to the API
-function createSession() {
-    var sessionHash = smite.genSig('createsession');
+function createSession(devId, authKey) {
+    var sessionHash = md5(devId + "createsession" + authKey + utcTime)
     request(baseUri + 'createsessionJson/' + devId + '/' + sessionHash + '/' + utcTime).pipe(parser.parse('session_id')).pipe(fs.createWriteStream('session.txt'));
 }
 
-// Function to get a md5 hash of the current method you
-// will be calling from the Smite API
-function genSig(methodName) {
-    var sigHash = md5(devId + methodName + authKey + utcTime);
-    return sigHash;
-}
 
-
-function getGods(){
-    var godsHash = genSig('getgods');
+function getGods(devId, authKey, sessionId){
+    var godsHash = md5(devId + "getgods" + authKey + utcTime);
     request(baseUri + "getgodsjson/" + devId + '/' + godsHash + '/' + sessionId + '/' + utcTime + '/' + 1).pipe(parser.parse("*")).pipe(parser.stringify()).pipe(fs.createWriteStream('gods.json'));
 }
 
 
-function getItems(){
-    var itemsHash = genSig('getitems');
+function getItems(devId, authKey, sessionId){
+    var itemsHash = md5(devId + "getitems" + authKey + utcTime)
     request(baseUri + "getitemsjson/" + devId + '/' + itemsHash + '/' + sessionId + '/' + utcTime + '/' + 1).pipe(parser.parse("*")).pipe(parser.stringify()).pipe(fs.createWriteStream('items.json'));
 }
 
-exports.utcTime = utcTime;
+exports.createSession = createSession;
 exports.getItems = getItems;
 exports.getGods = getGods;
-exports.genSig = genSig;
